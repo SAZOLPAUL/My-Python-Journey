f95f1b5ea8810a6f8e1461be05df69a8c0b06916
SAZOL PAUL <141328264+SAZOLPAUL@users.noreply.github.com>
2026-05-26 00:58:25 +0600

    Added Operators and Datatypes Practice
    Practiced arithmetic, comparison, and logical operators along with basic data types and error handling.

---
+++ 4th_Day.py
@@ -0,0 +1,105 @@
+##integer exampal
+age=19
+print(age,type(age))
+
+#floating point datatype
+height=5.8
+print(height,type(height))
+
+##string datatype example
+name="sazol"
+print(name,type(name))
+
+##boolean datatype
+is_true=True
+print(type(is_true))
+a=10
+b=10
+print(type(a==b))
+
+##common errors
+#result="pass" + 5 #error code 
+result="pass" + str(5)
+print(result)
+
+##arithmethic operator
+a=10
+b=5
+add_result=a+b #addition
+sub_result=a-b #substraction
+mul_result=a*b #multiplication
+div_result=a/b #division
+floor_div_result=a//b #floor division
+modulus_result=a%b #modlus operation
+exponent_result=a**b #exponentiation
+print(add_result)
+print(sub_result)
+print(mul_result)
+print(div_result)
+print(floor_div_result)
+print(modulus_result)
+print(exponent_result)
+
+##comrarsion operators
+a=10
+b=10
+print(a==b)
+
+str1="sazol"
+str2="sazol"
+print(str1==str2)
+
+##not eqal to !=
+str3="sazol"
+str4="Sazol"
+print(str3!=str4)
+
+
+##greter than
+num1=40
+num2=50
+print(num1>num2)
+
+
+##less than
+num3=60
+num4=50
+print(num3<num4)
+
+##greter then or equal to
+number1=50
+number2=50
+print(number1>=number2)
+
+##less then or eqal to
+number3=30
+number4=40
+print(number3<=number4)
+
+
+##logical operators (and,or,not)
+x=True
+y=True
+result=x and y
+print(result)
+
+result=x or y
+print(result)
+
+X=False
+Y=False
+Result=X and Y
+print(Result)
+ 
+Result=X or Y
+print(Result)
+
+z=True
+RESULT=not z
+print(RESULT)
+
+
+
+
+
+
