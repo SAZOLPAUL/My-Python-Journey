c591417eea989e37826d3e1165a8cc0e0410e012
SAZOL PAUL <141328264+SAZOLPAUL@users.noreply.github.com>
2026-05-27 01:00:05 +0600

    Refactor conditional statements for clarity

--- 5th_Day.py
+++ 5th_Day.py
@@ -1,34 +1,37 @@
-##if statement
-age=20
-if age>=18:
-  print("you are allowed to vote in the elections")
 
-##else statement
-age=16
-if age>=18:
-  print("you are allowed to vote in the elections.")
-else:
-  print("you are not allowed to vote in the elections!")
+  ## if statement
+age = 20
+if age >= 18:
+    print("You are allowed to vote in the elections.")
 
-##elif statement
-age=20
-if age<13:
-  print("you are a child")
-elif age<18:
-  print("you are a teenager")
+## else statement
+age = 16
+if age >= 18:
+    print("You are allowed to vote in the elections.")
 else:
-  print("you are a adult")
-  
-  
-##nested conditional statement
-num=int(input("enter the number: "))
-if num>0:
-  print("the number is positive")
-  if num%2==0:
-    print("the number is even")
-  else:
-    print("the number is odd")
-else:
-  print("the number is zero or nagative")
+    print("You are not allowed to vote in the elections!")
 
-  
+## elif statement
+age = 20
+if age < 13:
+    print("You are a child")
+elif age < 18:
+    print("You are a teenager")
+else:
+    print("You are an adult")
+
+## nested conditional statement
+num = int(input("Enter the number: "))
+
+if num > 0:
+    print("The number is positive")
+
+    if num % 2 == 0:
+        print("The number is even")
+    else:
+        print("The number is odd")
+
+elif num == 0:
+    print("The number is zero")
+else:
+    print("The number is negative")
